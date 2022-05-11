a9be27ec3056de442c0158b13e4eb58487a7cfdf
abdulgodultraksa <105394760+abdulgodultraksa@users.noreply.github.com>
2022-05-12 01:04:47 +0200

    Add files via upload

--- pacman.py
+++ pacman.py
@@ -70,20 +70,45 @@ def pacman():
 
     compteur=0
     coup = 0
+    vie = 3
     position_pacman=[17,14]
+    position_fantome_1=[1,1]
+    valeur_precedente_1 = plateau[position_fantome_1[0],position_fantome_1[1]]
     plateau[position_pacman[0],position_pacman[1]]=8
+    plateau[position_fantome_1[0],position_fantome_1[1]]=10
     print(afficher_plateau_lisible(plateau))
-    while condition_victoire(compteur) :
-        chemin = diffusion([position_pacman],plateau)
-        for k in range(len(chemin)):
-            case_arrivee = chemin[k]
-            compteur += plateau[case_arrivee[0],case_arrivee[1]]
-            plateau[position_pacman[0],position_pacman[1]] = 0
-            plateau[case_arrivee[0],case_arrivee[1]]=8
+    while condition_victoire(compteur) and vie != 0 :
+
+        chemin_f1 = fantome_1([position_fantome_1],plateau)
+        case_f1 = chemin_f1[0]
+        plateau[position_fantome_1[0], position_fantome_1[1]] = valeur_precedente_1
+        valeur_precedente_1 = plateau[case_f1[0], case_f1[1]]
+        plateau[case_f1[0], case_f1[1]] = 10
+        position_fantome_1 = case_f1
+
+        if position_fantome_1 == position_pacman:
+            vie += -1
+            chemin_pac = diffusion([position_pacman], plateau)
+            for k in range(5):
+                case_arrivee = chemin_pac[k]
+                compteur += plateau[case_arrivee[0], case_arrivee[1]]
+                plateau[position_pacman[0], position_pacman[1]] = 0
+                plateau[case_arrivee[0], case_arrivee[1]] = 8
+                position_pacman = case_arrivee
+                coup += 1
+            continue
+
+        chemin_pac = diffusion([position_pacman],plateau)
+        for k in range(2):
+            case_arrivee = chemin_pac[k]
+            compteur += plateau[case_arrivee[0], case_arrivee[1]]
+            plateau[position_pacman[0], position_pacman[1]] = 0
+            plateau[case_arrivee[0], case_arrivee[1]] = 8
             position_pacman = case_arrivee
             coup += 1
             t.sleep(1)
             print(afficher_plateau_lisible(plateau))
+
     print( "BIEN JOUE PETIT CHENAPANT", coup, afficher_plateau_lisible(plateau) )
         
     
@@ -302,7 +327,29 @@ def diffusion(liste_cases_etudiees,carte):
     
         
     
-                    
+def fantome_1(liste_cases_etudiees, carte):
+    liste_cases_future = []
+    plateau = carte.copy()
+    for case_precedente in liste_cases_etudiees:
+        for deplacement in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
+            case_suivante = np.array(case_precedente) + np.array(deplacement)
+            if plateau[case_suivante[0], case_suivante[1]] == 0:
+                plateau[case_suivante[0], case_suivante[1]] = 2
+                liste_cases_future.append(case_suivante)
+            elif plateau[case_suivante[0], case_suivante[1]] == 8:
+                for case_remplie in liste_cases_future:
+                    plateau[case_remplie[0], case_remplie[1]] = 0
+                return [case_suivante]
+
+    chemin_retour = diffusion(liste_cases_future, plateau)
+    for deplacement in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
+        case_precedente = np.array(chemin_retour[0]) + np.array(deplacement)
+        if plateau[case_precedente[0], case_precedente[1]] == 2:
+            for case_remplie in liste_cases_future:
+                plateau[case_remplie[0], case_remplie[1]] = 0
+            chemin_retour.insert(0, case_precedente)
+            return chemin_retour
+
     
 
 
